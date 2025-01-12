b0651e72635ef935b6a1789f2a3ab795bfefaf03
BaharAlmasiMoghaddam <baharalmasiimoghaddam@gmail.com>
2025-01-12 14:34:23 +0330

    Compelete-Q3-numbers!

--- JS-13-number.js
+++ JS-13-number.js
@@ -9,3 +9,19 @@ num2 = 3
 */
 
 //! Answer:
+
+function generateSequence() {
+  let num1 = Math.floor(Math.random() * 16);
+  let num2 = Math.floor(Math.random() * 16);
+
+  let sequence = [num1, num2];
+
+  for (let i = 2; i < 10; i++) {
+    let nextNum = (sequence[i - 1] + sequence[i - 2]) * 2;
+    sequence.push(nextNum);
+  }
+
+  return sequence;
+}
+
+console.log(generateSequence());
