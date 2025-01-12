3775db81a273a838d3aa38af6f32e35640000789
BaharAlmasiMoghaddam <baharalmasiimoghaddam@gmail.com>
2025-01-12 14:43:11 +0330

    copelete-Q4-other!

--- JS-13-Other.js
+++ JS-13-Other.js
@@ -1,18 +1,30 @@
 // ? Question-1:select section counter by class name(.cards__card__img__icon) and save in variable "counterEl":
+
 // !Answer:
+let counterEl = document.querySelector(".cards__card__img__icon");
 
 // ? Question-2:select item counter by id(#counter) and save in variable "counter":
+
 // !Answer:
+let counter = document.getElementById("counter");
 
 // ? Question-3:select section (.cards) and save in variable "card":
+
 // !Answer:
+let card = document.querySelector(".cards");
 
 // ? Question-4:select section (.cards__card) and save in variable "cards":
+
 // !Answer:
+let cards = document.querySelector(".cards__card");
 
 // ? Question-5:select button in class cards_card_footer and save in variable "btnEl":
+
 // !Answer
+let btnEls = document.querySelector(".cards__card__footer__btn button");
 
 // ? Question-6:select the img that has attribute "man" and show in console:
-// !Answer
-console.log('hello');
+// !Answer:
+let imgEl = document.querySelector('img[alt="man"]');
+console.log(imgEl);
+// console.log("hello");
